297f5363424183868ae990695846f8a47bfed481
Wanderson Alves <wandersonalvesferreira2012@gmail.com>
2019-08-12 01:40:55 -0300

    feat: add RecetItem from json

--- lib/views/widgets/RecentCards.dart
+++ lib/views/widgets/RecentCards.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_sleep_app/common/Utils.dart';
+import 'package:flutter_sleep_app/model/RecommendedItem.dart';
 import 'package:flutter_sleep_app/styles/Colors.dart';
 import 'package:flutter_sleep_app/styles/Typography.dart';
 import 'package:flutter_sleep_app/widgets/AppCard.dart';
@@ -20,73 +23,53 @@ class RecentCardDefault extends StatelessWidget {
 }
 
 class RecentCards extends StatelessWidget {
+  List<RecommendedItem> parseJSON(String res) {
+    if (res == null) {
+      return [];
+    }
+    final parsed = json.decode(res.toString()).cast<Map<String, dynamic>>();
+    return parsed
+        .map<RecommendedItem>((json) => RecommendedItem.fromJSON(json))
+        .toList();
+  }
+
+  FutureBuilder<String> recentCardsFutureBuilder(BuildContext context) {
+    return FutureBuilder(
+      future: DefaultAssetBundle.of(context)
+          .loadString('assets/json/recommendedItems.json'),
+      builder: (context, snapshot) {
+        List<RecommendedItem> items = parseJSON(snapshot.data.toString());
+        return items.isNotEmpty
+            ? Container(
+              child: Wrap(
+              direction: Axis.horizontal,
+              runSpacing: 20,
+              spacing: 20,
+              children: items.map((item) {
+                  return RecentCardDefault(
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(item.title, style: TypographyStyles.cardTitle),
+                        Expanded(child: SizedBox()),
+                        Row(
+                          children: Utils.childrenWithSeparator('row', size: 5, children: item.icons)
+                        )
+                      ],
+                    ),
+                    color: item.color,
+                  );
+                }).toList()),
+              )
+            : SizedBox();
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
-      child: Wrap(
-        direction: Axis.horizontal,
-        runSpacing: 20,
-        spacing: 20,
-        children: [
-          RecentCardDefault(
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Calming Sounds', style: TypographyStyles.cardTitle),
-                Expanded(child: SizedBox()),
-                Icon(Icons.headset, color: Colors.white)
-              ],
-            )
-          ),
-          RecentCardDefault(
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Insomnia', style: TypographyStyles.cardTitle),
-                Expanded(child: SizedBox()),
-                Row(
-                  children: [
-                    Icon(Icons.movie, color: Colors.white),
-                    SizedBox(width: 5),
-                    Icon(Icons.warning, color: Colors.white),
-                    SizedBox(width: 5),
-                    Icon(Icons.library_music, color: Colors.white),
-                  ],
-                )
-              ],
-            ),
-            color: Colors.redAccent,
-          ),
-          RecentCardDefault(
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('For Children', style: TypographyStyles.cardTitle),
-                Expanded(child: SizedBox()),
-                Row(
-                  children: [
-                    Icon(Icons.child_friendly, color: Colors.white),
-                    SizedBox(width: 5),
-                    Icon(Icons.child_care, color: Colors.white),
-                  ],
-                )
-              ],
-            ),
-            color: Colors.teal,
-          ),
-          RecentCardDefault(
-            Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text('Tips for Sleeping', style: TypographyStyles.cardTitle),
-                Expanded(child: SizedBox()),
-                Icon(Icons.do_not_disturb_on, color: Colors.white)
-              ],
-            ),
-            color: Colors.orange,
-          ),
-        ],
-      ),
+      child: recentCardsFutureBuilder(context)
     );
   }
 }
